9187c21360ec9660f97e28f66fc5735c06ab28ee
Timekl <1264166219@qq.com>
2022-03-03 01:03:57 +0800

    1/y/13.45%

---
+++ 1.两数之和.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        int r = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target) 
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    r = 1;
+                    break;
+                }
+            }
+                if (r) {break;}
+            
+        }
+        return ans;
+
+    }
+};
+// @lc code=end
+
