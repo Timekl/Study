4fcbfcf22360ab4273a1759c4abd32324437bf2d
Timekl <1264166219@qq.com>
2022-03-03 01:19:36 +0800

    优化之后有更少的数学运算来提高效率

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -12,12 +12,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>ans;
+        int oth = 0;
         int r = 0;
         for (int i = 0; i < nums.size() - 1; i++)
         {
+            oth = target - nums[i];
             for (int j = i + 1; j < nums.size(); j++)
             {
-                if (nums[i] + nums[j] == target) 
+                if (nums[j] == oth) 
                 {
                     ans.push_back(i);
                     ans.push_back(j);
